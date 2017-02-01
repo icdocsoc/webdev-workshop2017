587073086d043e93c7e60000096a4fbfa4ffd424
Szilveszter Székely <szilveszter.szekely15@imperial.ac.uk>
2017-02-01 22:15:53 +0000

    routes: Remove template routes

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
-var index = require('./routes/index')
-var users = require('./routes/users')
+var routes = require('./routes')
 
 var app = express()
 
@@ -16,8 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index)
-app.use('/users', users)
+app.use('/', routes)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
