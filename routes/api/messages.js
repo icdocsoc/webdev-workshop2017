b6d2735367fd5c24fd319cdc0f4ece60445c6b05
Szilveszter Székely <szilveszter.szekely15@imperial.ac.uk>
2017-02-02 01:20:30 +0000

    routes/api/messages.js: Use mongo as backing store

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,15 +1,25 @@
 var express = require('express')
 var router = express.Router()
 
-var messages = []
+var Message = require('../../models/message')
 
-router.get('/', function (req, res) {
-  res.json(messages)
+router.get('/', function (req, res, next) {
+  Message.find({}).exec(function (err, messages) {
+    if (err) {
+      return next(err)
+    }
+    res.json(messages)
+  })
 })
 
-router.post('/', function (req, res) {
-  messages.unshift(req.body)
-  res.status(200).end()
+router.post('/', function (req, res, next) {
+  var msg = new Message(req.body)
+  msg.save(function (err) {
+    if (err) {
+      next(err)
+    }
+    res.status(200).end()
+  })
 })
 
 module.exports = router
