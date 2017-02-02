1f8c1ac381761c629a388e3ae8e8a6d85e11c346
Szilveszter Székely <szilveszter.szekely15@imperial.ac.uk>
2017-02-02 00:40:23 +0000

    public/javascripts/app.js: Remove sample data

--- week3/public/javascripts/app.js
+++ week3/public/javascripts/app.js
@@ -3,33 +3,7 @@ var angular = window.angular
 var app = angular.module('chatapp', [])
 
 app.service('MessageService', function () {
-  var messages = [
-    {
-      sender: 'Szilveszter',
-      text: 'Cras justo odio',
-      date: '2017-02-02 18:09'
-    },
-    {
-      sender: 'Abhinav',
-      text: 'Dapibus ac facilisis in',
-      date: '2017-02-02 18:08'
-    },
-    {
-      sender: 'Abhinav',
-      text: 'Morbi leo risus',
-      date: '2017-02-02 18:07'
-    },
-    {
-      sender: 'Szilveszter',
-      text: 'Porta ac consectetur ac',
-      date: '2017-02-02 18:06'
-    },
-    {
-      sender: 'Abhinav',
-      text: 'Vestibulum at eros',
-      date: '2017-02-02 18:05'
-    }
-  ]
+  var messages = []
 
   this.get = function () {
     return messages
