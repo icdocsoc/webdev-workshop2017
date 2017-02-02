12d8aa59b89237cb78a62dec1ffac9b9d763ceb4
Szilveszter Székely <szilveszter.szekely15@imperial.ac.uk>
2017-02-02 00:20:38 +0000

    public/javascripts/app.js: Move sample data to controller

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,4 +3,31 @@ var angular = window.angular
 var app = angular.module('chatapp', [])
 
 app.controller('ChatController', function ($scope) {
+  $scope.messages = [
+    {
+      sender: 'Szilveszter',
+      text: 'Cras justo odio',
+      date: '2017-02-02 18:09'
+    },
+    {
+      sender: 'Abhinav',
+      text: 'Dapibus ac facilisis in',
+      date: '2017-02-02 18:08'
+    },
+    {
+      sender: 'Abhinav',
+      text: 'Morbi leo risus',
+      date: '2017-02-02 18:07'
+    },
+    {
+      sender: 'Szilveszter',
+      text: 'Porta ac consectetur ac',
+      date: '2017-02-02 18:06'
+    },
+    {
+      sender: 'Abhinav',
+      text: 'Vestibulum at eros',
+      date: '2017-02-02 18:05'
+    }
+  ]
 })
